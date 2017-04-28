f990a3e9d6c519ebc200ec99cfcc3d6797816d26
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2017-04-28 15:17:03 +0300

    Initial commit

---
+++ app/src/main/java/com/kouyemek/omrumbakitemiz/kouyemekandroid/Yemek.java
@@ -0,0 +1,86 @@
+package com.kouyemek.omrumbakitemiz.kouyemekandroid;
+
+public class Yemek {
+
+    private String gun;
+    private String corba;
+    private String corbaKalori;
+    private String anaYemek;
+    private String anaYemekKalori;
+    private String yardimciYemek;
+    private String yardimciYemekKalori;
+    private String salataTatli;
+    private String salataTatliKalori;
+
+    public String getGun() {
+        return gun;
+    }
+
+    public void setGun(String gun) {
+        this.gun = gun;
+    }
+
+    public String getCorba() {
+        return corba;
+    }
+
+    public void setCorba(String corba) {
+        this.corba = corba;
+    }
+
+    public String getCorbaKalori() {
+        return corbaKalori;
+    }
+
+    public void setCorbaKalori(String corbaKalori) {
+        this.corbaKalori = corbaKalori;
+    }
+
+    public String getAnaYemek() {
+        return anaYemek;
+    }
+
+    public void setAnaYemek(String anaYemek) {
+        this.anaYemek = anaYemek;
+    }
+
+    public String getAnaYemekKalori() {
+        return anaYemekKalori;
+    }
+
+    public void setAnaYemekKalori(String anaYemekKalori) {
+        this.anaYemekKalori = anaYemekKalori;
+    }
+
+    public String getYardimciYemek() {
+        return yardimciYemek;
+    }
+
+    public void setYardimciYemek(String yardimciYemek) {
+        this.yardimciYemek = yardimciYemek;
+    }
+
+    public String getYardimciYemekKalori() {
+        return yardimciYemekKalori;
+    }
+
+    public void setYardimciYemekKalori(String yardimciYemekKalori) {
+        this.yardimciYemekKalori = yardimciYemekKalori;
+    }
+
+    public String getSalataTatli() {
+        return salataTatli;
+    }
+
+    public void setSalataTatli(String salataTatli) {
+        this.salataTatli = salataTatli;
+    }
+
+    public String getSalataTatliKalori() {
+        return salataTatliKalori;
+    }
+
+    public void setSalataTatliKalori(String salataTatliKalori) {
+        this.salataTatliKalori = salataTatliKalori;
+    }
+}
